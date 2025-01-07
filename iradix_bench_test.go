f19863c7a201abf1cb4508ea127eb4fa7de8ef50
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2025-01-07 17:10:17 +0530

    bulk insert

--- iradix_bench_test.go
+++ iradix_bench_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func benchInsertTxn(b *testing.B, batchSize int, track bool) {
+func benchBulkInsertTxn(b *testing.B, batchSize int, track bool) {
 	r := New()
 
 	b.ResetTimer()
@@ -28,37 +28,37 @@ func benchInsertTxn(b *testing.B, batchSize int, track bool) {
 	}
 }
 
-func Benchmark10InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 10, true)
+func Benchmark10BulkInsertTxnTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 10, true)
 }
-func Benchmark10InsertTxnNoTrack(b *testing.B) {
-	benchInsertTxn(b, 10, false)
-}
-
-func Benchmark100InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 100, true)
-}
-func Benchmark100InsertTxnNoTrack(b *testing.B) {
-	benchInsertTxn(b, 100, false)
+func Benchmark10BulkInsertTxnNoTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 10, false)
 }
 
-func Benchmark1000InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 1000, true)
+func Benchmark100BulkInsertTxnTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 100, true)
 }
-func Benchmark1000InsertTxnNoTrack(b *testing.B) {
-	benchInsertTxn(b, 1000, false)
-}
-
-func Benchmark10000InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 10000, true)
-}
-func Benchmark10000InsertTxnNoTrack(b *testing.B) {
-	benchInsertTxn(b, 10000, false)
+func Benchmark100BulkInsertTxnNoTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 100, false)
 }
 
-func Benchmark100000InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 100000, true)
+func Benchmark1000BulkInsertTxnTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 1000, true)
 }
-func Benchmark100000InsertTxnNoTrack(b *testing.B) {
-	benchInsertTxn(b, 100000, false)
+func Benchmark1000BulkInsertTxnNoTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 1000, false)
+}
+
+func Benchmark10000BulkInsertTxnTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 10000, true)
+}
+func Benchmark10000BulkInsertTxnNoTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 10000, false)
+}
+
+func Benchmark100000BulkInsertTxnTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 100000, true)
+}
+func Benchmark100000BulkInsertTxnNoTrack(b *testing.B) {
+	benchBulkInsertTxn(b, 100000, false)
 }
