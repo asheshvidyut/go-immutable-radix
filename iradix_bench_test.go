ff2872600dd3721ebe5c685863790cb3eb6a3826
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2025-01-11 15:02:49 +0530

    remove track mutate

--- iradix_bench_test.go
+++ iradix_bench_test.go
@@ -21,51 +21,32 @@ func benchBulkInsertTxn(b *testing.B, batchSize int, track bool) {
 	for i := 0; i < b.N; i++ {
 		start := i * batchSize
 		txn := r.Txn()
-		txn.TrackMutate(track)
 		txn.BulkInsert(keys[start:start+batchSize], values[start:start+batchSize])
 		r = txn.Commit()
 	}
 
 }
 
-func Benchmark10BulkInsertTxnTrack(b *testing.B) {
-	benchBulkInsertTxn(b, 10, true)
-}
 func Benchmark10BulkInsertTxnNoTrack(b *testing.B) {
 	benchBulkInsertTxn(b, 10, false)
 }
 
-func Benchmark100BulkInsertTxnTrack(b *testing.B) {
-	benchBulkInsertTxn(b, 100, true)
-}
 func Benchmark100BulkInsertTxnNoTrack(b *testing.B) {
 	benchBulkInsertTxn(b, 100, false)
 }
 
-func Benchmark1000BulkInsertTxnTrack(b *testing.B) {
-	benchBulkInsertTxn(b, 1000, true)
-}
 func Benchmark1000BulkInsertTxnNoTrack(b *testing.B) {
 	benchBulkInsertTxn(b, 1000, false)
 }
 
-func Benchmark10000BulkInsertTxnTrack(b *testing.B) {
-	benchBulkInsertTxn(b, 10000, true)
-}
 func Benchmark10000BulkInsertTxnNoTrack(b *testing.B) {
 	benchBulkInsertTxn(b, 10000, false)
 }
 
-func Benchmark100000BulkInsertTxnTrack(b *testing.B) {
-	benchBulkInsertTxn(b, 100000, true)
-}
 func Benchmark100000BulkInsertTxnNoTrack(b *testing.B) {
 	benchBulkInsertTxn(b, 100000, false)
 }
 
-func Benchmark1000000BulkInsertTxnTrack(b *testing.B) {
-	benchBulkInsertTxn(b, 1000000, true)
-}
 func Benchmark1000000BulkInsertTxnNoTrack(b *testing.B) {
 	benchBulkInsertTxn(b, 1000000, false)
 }
