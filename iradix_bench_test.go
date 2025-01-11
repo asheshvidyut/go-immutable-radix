2f8e7101681cdfd968efe5e0bd7fb4dd8dce470f
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2025-01-11 15:04:04 +0530

    remove track mutate

--- iradix_bench_test.go
+++ iradix_bench_test.go
@@ -31,44 +31,26 @@ func benchInsertTxn(b *testing.B, batchSize int, track bool) {
 	}
 }
 
-func Benchmark10InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 10, true)
-}
 func Benchmark10InsertTxnNoTrack(b *testing.B) {
 	benchInsertTxn(b, 10, false)
 }
 
-func Benchmark100InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 100, true)
-}
 func Benchmark100InsertTxnNoTrack(b *testing.B) {
 	benchInsertTxn(b, 100, false)
 }
 
-func Benchmark1000InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 1000, true)
-}
 func Benchmark1000InsertTxnNoTrack(b *testing.B) {
 	benchInsertTxn(b, 1000, false)
 }
 
-func Benchmark10000InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 10000, true)
-}
 func Benchmark10000InsertTxnNoTrack(b *testing.B) {
 	benchInsertTxn(b, 10000, false)
 }
 
-func Benchmark100000InsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 100000, true)
-}
 func Benchmark100000InsertTxnNoTrack(b *testing.B) {
 	benchInsertTxn(b, 100000, false)
 }
 
-func Benchmark1000000BulkInsertTxnTrack(b *testing.B) {
-	benchInsertTxn(b, 1000000, true)
-}
 func Benchmark1000000BulkInsertTxnNoTrack(b *testing.B) {
 	benchInsertTxn(b, 1000000, false)
 }
